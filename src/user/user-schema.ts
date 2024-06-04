2452f2445a8f50cda67fb5c3a93af0c37c9c523f
nanorisrael <israel.nanor@compozelabs.com>
2024-06-04 13:45:09 +0000

    Add user schema

---
+++ src/user/user-schema.ts
@@ -0,0 +1,20 @@
+// models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
