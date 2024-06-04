fe13118ef03054e2c1d78234a098a1f26127d28d
nanorisrael <israel.nanor@compozelabs.com>
2024-06-04 14:58:59 +0000

    Add read me file

---
+++ README.md
@@ -0,0 +1,15 @@
+# Unique store backend documentation
+### Author: Israel Nanor
+
+
+## TechStack
+- Nodejs
+- Typescript
+- Mongodb with mongoose
+
+## Run
+npm run dev
+
+## Test
+npm run test
+
---
+++ src/utils.ts
@@ -0,0 +1,12 @@
+// import mongoose from "mongoose";
+
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// } as mongoose.ConnectOptions);
+
+// export const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function () {
+//   console.log("Connected to MongoDB");
+// });
