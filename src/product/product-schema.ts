0eafec9ab54e09c0ad60f010463aa94db579be49
nanorisrael <israel.nanor@compozelabs.com>
2024-06-05 22:35:28 +0000

    Add product schema

---
+++ src/product/product-schema.ts
@@ -0,0 +1,24 @@
+// models/User.js
+import mongoose, { Types } from "mongoose";
+
+export interface ProductDoc extends Document {
+  _id: Types.ObjectId;
+  name: string;
+  stock: number;
+  price: number;
+  createdAt: Date;
+}
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  stock: Number,
+  price: Number,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Product = mongoose.model<ProductDoc>("Product", productSchema);
+
+export default Product;
