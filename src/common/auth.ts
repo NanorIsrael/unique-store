10ca0aec06a976e8c5f7c1aca1b0f95731ff195c
nanorisrael <israel.nanor@compozelabs.com>
2024-06-10 04:05:32 +0000

    Add controller

--- src/common/auth.ts
+++ src/common/auth.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import TokenService from "../auth/auth-token-service";
 import BadRequestError from "./error-handlers/badrequest";
 import NotAuthorisedError from "./error-handlers/unauthorised";
+import AdminService from "../user/admin/admin.service";
 
 export function getAuthHeader(headers: {
   [key: string]: string | string[] | undefined;
@@ -12,6 +13,7 @@ export function getAuthHeader(headers: {
   }
   return authHeader.split(" ")[1];
 }
+
 export async function verifyUser(
   req: Request,
   res: Response,
@@ -34,3 +36,30 @@ export async function verifyUser(
   }
   next();
 }
+
+export async function verifyAdminUser(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const jwt = getAuthHeader(req.headers);
+    if (!jwt) {
+      throw new BadRequestError("access token not found.");
+    }
+    const tokenService = new TokenService();
+    const userId = await tokenService.verifyToken(jwt);
+    const adminService = new AdminService();
+    const userId = await adminService.findAdminById(userId);
+    if (!userId) {
+      throw new NotAuthorisedError();
+    }
+
+    req.body = { ...req.body, userId };
+  } catch (error) {
+    next(error);
+  }
+  next();
+}
+
+
